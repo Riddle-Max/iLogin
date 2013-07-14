32f403ae13a8d1663a8bb191f41cc630cc106a01
leibiao <450569689@qq.com>
2013-07-14 19:58:31 +0800

    把ApplicationName更改为iLogin

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
     QSingleApplication a(argc, argv);
-    a.setApplicationName("iClient");
+    a.setApplicationName("iLogin");
     a.setApplicationVersion("v1.0");
     a.setOrganizationName("Central South University");
     SystemTrayIcon app;
